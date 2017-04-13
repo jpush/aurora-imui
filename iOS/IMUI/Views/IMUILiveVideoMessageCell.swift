61866a4b1118729e8085cb7cc6201910b000a6a5
huangminlinux <380108184@qq.com>
2017-04-13 16:07:43 +0800

    add video message cell

---
+++ iOS/IMUI/Views/IMUILiveVideoMessageCell.swift
@@ -0,0 +1,49 @@
+//
+//  IMUIVideoMessageCell.swift
+//  IMUIChat
+//
+//  Created by oshumini on 2017/4/1.
+//  Copyright © 2017年 HXHG. All rights reserved.
+//
+
+import UIKit
+
+class IMUIVideoMessageCell: IMUIBaseMessageCell {
+
+
+  var videoView = UIView()
+  var videoReader = IMUIVideoFileLoader()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    bubbleView.addSubview(self.videoView)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+  }
+  
+  func didDisAppearCell() {
+    self.videoReader.isNeedToStopVideo = true
+  }
+  
+  override func presentCell(with message: IMUIMessageModel, delegate: IMUIMessageMessageCollectionViewDelegate?) {
+    super.presentCell(with: message, delegate: delegate)
+    self.layoutVideo(with: message.videoPath!)
+    let layout = message.layout as! IMUIMessageCellLayout
+    self.videoView.frame = UIEdgeInsetsInsetRect(CGRect(origin: CGPoint.zero, size: layout.bubbleFrame.size), layout.bubbleContentInset)
+  }
+  
+  func layoutVideo(with videoPath: String) {
+    self.videoView.layer.contents = nil
+    self.videoReader.loadVideoFile(with: URL(fileURLWithPath: videoPath)) { (videoFrameImage) in
+      DispatchQueue.main.async {
+        self.videoView.layer.contents = videoFrameImage
+      }
+    }
+  }
+}
--- iOS/IMUI/Views/IMUIVideoMessageCell.swift
+++ iOS/IMUI/Views/IMUIVideoMessageCell.swift
@@ -2,7 +2,7 @@
 //  IMUIVideoMessageCell.swift
 //  IMUIChat
 //
-//  Created by oshumini on 2017/4/1.
+//  Created by oshumini on 2017/4/13.
 //  Copyright © 2017年 HXHG. All rights reserved.
 //
 
@@ -10,40 +10,12 @@ import UIKit
 
 class IMUIVideoMessageCell: IMUIBaseMessageCell {
 
-
-  var videoView = UIView()
-  var videoReader = IMUIVideoFileLoader()
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    bubbleView.addSubview(self.videoView)
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  override func layoutSubviews() {
-    super.layoutSubviews()
-  }
-  
-  func didDisAppearCell() {
-    self.videoReader.isNeedToStopVideo = true
-  }
-  
-  override func presentCell(with message: IMUIMessageModel, delegate: IMUIMessageMessageCollectionViewDelegate?) {
-    super.presentCell(with: message, delegate: delegate)
-    self.layoutVideo(with: message.videoPath!)
-    let layout = message.layout as! IMUIMessageCellLayout
-    self.videoView.frame = UIEdgeInsetsInsetRect(CGRect(origin: CGPoint.zero, size: layout.bubbleFrame.size), layout.bubbleContentInset)
-  }
-  
-  func layoutVideo(with videoPath: String) {
-    self.videoView.layer.contents = nil
-    self.videoReader.loadVideoFile(with: URL(fileURLWithPath: videoPath)) { (videoFrameImage) in
-      DispatchQueue.main.async {
-        self.videoView.layer.contents = videoFrameImage
-      }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
     }
-  }
+    */
+
 }
