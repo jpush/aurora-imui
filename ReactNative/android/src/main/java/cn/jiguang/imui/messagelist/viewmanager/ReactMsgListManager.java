0e38d92f5cd083c64983389b8fa961a315cfb034
KenChoi <caiyaoguan@caiyaoguandeMacBook-Pro.local>
2018-03-27 16:25:52 +0800

    fix image scale bug

--- ReactNative/android/src/main/java/cn/jiguang/imui/messagelist/viewmanager/ReactMsgListManager.java
+++ ReactNative/android/src/main/java/cn/jiguang/imui/messagelist/viewmanager/ReactMsgListManager.java
@@ -7,7 +7,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -15,10 +17,12 @@ import android.hardware.SensorManager;
 import android.media.AudioManager;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
@@ -26,6 +30,8 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReadableArray;
@@ -152,6 +158,12 @@ public class ReactMsgListManager extends ViewGroupManager<PullToRefreshLayout> i
         // Use default layout
         MsgListAdapter.HoldersConfig holdersConfig = new MsgListAdapter.HoldersConfig();
         ImageLoader imageLoader = new ImageLoader() {
+
+            final float MIN_WIDTH = 60 * density;
+            final float MAX_WIDTH = 200 * density;
+            final float MIN_HEIGHT = 60 * density;
+            final float MAX_HEIGHT = 200 * density;
+
             @Override
             public void loadAvatarImage(ImageView avatarImageView, String string) {
                 int resId = IdHelper.getDrawable(reactContext, string);
@@ -171,8 +183,61 @@ public class ReactMsgListManager extends ViewGroupManager<PullToRefreshLayout> i
                 Glide.with(reactContext)
                         .asBitmap()
                         .load(string)
-                        .apply(new RequestOptions().placeholder(IdHelper.getDrawable(reactContext, "aurora_picture_not_found")))
-                        .into(new ImageTarget(imageView, density));
+                        .apply(new RequestOptions().fitCenter().placeholder(R.drawable.aurora_picture_not_found))
+                        .into(new SimpleTarget<Bitmap>() {
+                            @Override
+                            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                                int imageWidth = resource.getWidth();
+                                int imageHeight = resource.getHeight();
+
+                                // 裁剪 bitmap
+                                float width, height;
+                                if (imageWidth > imageHeight) {
+                                    if (imageWidth > MAX_WIDTH) {
+                                        float temp = MAX_WIDTH / imageWidth * imageHeight;
+                                        height = temp > MIN_HEIGHT ? temp : MIN_HEIGHT;
+                                        width = MAX_WIDTH;
+                                    } else if (imageWidth < MIN_WIDTH) {
+                                        float temp = MIN_WIDTH / imageWidth * imageHeight;
+                                        height = temp < MAX_HEIGHT ? temp : MAX_HEIGHT;
+                                        width = MIN_WIDTH;
+                                    } else {
+                                        float ratio = imageWidth / imageHeight;
+                                        if (ratio > 3) {
+                                            ratio = 3;
+                                        }
+                                        height = imageHeight * ratio;
+                                        width = imageWidth;
+                                    }
+                                } else {
+                                    if (imageHeight > MAX_HEIGHT) {
+                                        float temp = MAX_HEIGHT / imageHeight * imageWidth;
+                                        width = temp > MIN_WIDTH ? temp : MIN_WIDTH;
+                                        height = MAX_HEIGHT;
+                                    } else if (imageHeight < MIN_HEIGHT) {
+                                        float temp = MIN_HEIGHT / imageHeight * imageWidth;
+                                        width = temp < MAX_WIDTH ? temp : MAX_WIDTH;
+                                        height = MIN_HEIGHT;
+                                    } else {
+                                        float ratio = imageHeight / imageWidth;
+                                        if (ratio > 3) {
+                                            ratio = 3;
+                                        }
+                                        width = imageWidth * ratio;
+                                        height = imageHeight;
+                                    }
+                                }
+                                ViewGroup.LayoutParams params = imageView.getLayoutParams();
+                                params.width = (int) width;
+                                params.height = (int) height;
+                                imageView.setLayoutParams(params);
+                                Matrix matrix = new Matrix();
+                                float scaleWidth = width / imageWidth;
+                                float scaleHeight = height / imageHeight;
+                                matrix.postScale(scaleWidth, scaleHeight);
+                                imageView.setImageBitmap(Bitmap.createBitmap(resource, 0, 0, imageWidth, imageHeight, matrix, true));
+                            }
+                        });
             }
 
             @Override
